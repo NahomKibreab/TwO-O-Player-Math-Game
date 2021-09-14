6f4962bd3448df56b082a88ae349c332293b2ae3
Nahom Mehanzel <nknahom@gmail.com>
2021-09-14 01:39:22 +0000

    Add while loop functionality

--- game.rb
+++ game.rb
@@ -1,11 +1,12 @@
 class Game
-  attr_accessor :current_player
+  attr_reader :current_player
+
   def initialize(p1,p2)
     @number1 = 0
     @number2 = 0
-    @current_player = p1
     @p1 = p1
     @p2 = p2
+    @current_player = nil
   end
 
   def random_question
@@ -14,29 +15,48 @@ class Game
     "What does #{@number1} plus #{@number2} equal?"
   end
 
-  def random_number
-    rand(1..20)
+  def current_player
+    if(@current_player == nil)
+      @current_player = @p1
+    else
+      @current_player = @current_player == @p1 ? @p2 : @p1
+    end
   end
 
   def check_answer?(answer)
-    response(answer == (@number1 + @number2))
+    result(answer.to_i == (@number1 + @number2))
   end
-
+  
   def result(answer)
     if(answer)
       puts "YES! You are correct."
     else
       puts "Seriously? No!"
+      @current_player.lost_life
     end
     puts "P1: #{@p1.life}/3 vs P2: #{@p2.life}/3"
+    game_round
   end
+  
+  
+  protected
 
+  def winner
+    @p1.life != 0 ? @p1 : @p2
+  end
+  
   def game_round
     if(@p1.life == 0 || @p2.life == 0)
+      puts "#{winner.name} wins with a score of #{winner.life}/3"
       puts "----- GAME OVER -----"
       puts "Good bye!"
+      exit(1)
     else
       puts "----- NEW TURN -----"
     end
+  end
+  
+  def random_number
+    rand(1..20)
   end
 end
--- main.rb
+++ main.rb
@@ -2,12 +2,20 @@ require './player'
 require './game'
 
 class MathGame
-  attr_accessor :game
   def initialize
     @player1 = Player.new("Player 1")
     @player2 = Player.new("Player 2")
     @game = Game.new(@player1, @player2)
   end
+
+  def play_game
+    while @player1.life > 0 && @player2.life > 0 do
+      puts "#{@game.current_player.name}: #{@game.random_question}"
+      @game.check_answer?(gets.chomp)
+    end 
+  end
+  
 end
 
 start_game = MathGame.new
+start_game.play_game
